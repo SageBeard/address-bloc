8a4a45ea405fd36896d3369bc572e19067eb360e
Sage Beard <sagek711@gmail.com>
2017-02-11 16:03:31 -0800

    assn 8 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -64,4 +64,13 @@ def binary_search(name)
     end
     return nil
   end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name === name
+        return entry
+      end
+    end
+    return nil
+  end
 end
